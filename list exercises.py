2691518f23432f04eb8144215863a2ca08d4ad68
Steve Tangsombatvisit <stang@netflix.comm>
2018-05-07 20:40:20 -0700

    add more detailed questions

--- list exercises.py
+++ list exercises.py
@@ -2,22 +2,19 @@
 
 # Create By Steve Tangsombatvisit, March-2017 for Coder Dojo Beginners Python
 
-#1. Create a list of your five favorite foods.
-
+#1. Create a list of your five favorite foods. Store this list as a variable called ("favorite_foods")
 
 #2. Print the entire list.
 
-
 #3. Print the third item in your list.
-
 
 #4. Add a few more foods to your favorite foods list! (Hint, there are two ways to do this. Can you do both?)
 
-
 #5. Print out the size of your list.
 
-
-
+#6. Using user input, write a program that asks the user to enter in his or her five favorite foods. Print this list out.
 
 #BONUS
 # Can you write some code that randomly selects a food from your list and print it out?
+
+# Can you do some research and find out how to remove an item from a list?
